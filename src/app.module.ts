8279b2e5b68c2d4688d3d0ee47722826f98ae934
Diego EFC <43099313+DiegoEmanuel@users.noreply.github.com>
2025-01-31 18:10:57 -0300

    Configure TypeORM with SQLite database and update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DevelopersModule } from './developers/developers.module';
 
+
 @Module({
-  imports: [DevelopersModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    DevelopersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
+  
 })
 export class AppModule {}
