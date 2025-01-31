268b1d8ec8525b1ed73e9e258ffbaeee6bedc47b
Diego EFC <43099313+DiegoEmanuel@users.noreply.github.com>
2025-01-31 18:01:57 -0300

    Add HTTP requests and update DTO for developers module

--- src/developers/dto/update-developer.dto.ts
+++ src/developers/dto/update-developer.dto.ts
@@ -1,4 +1,5 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateDeveloperDto } from './create-developer.dto';
 
+//ta dizendo que é pra aproveitar oq tem lá na create-developer.dto só que é opcional
 export class UpdateDeveloperDto extends PartialType(CreateDeveloperDto) {}
